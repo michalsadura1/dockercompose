43ae80bd616c5b4afbc768edaa4821d77103f407
michal <michal100000@gmail.com>
2020-12-07 23:47:17 +0100

    potgres infra docker-compose push

---
+++ create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#create volumes
+mkdir /postgres_data
+chown 999:999 /postgres_data
+ls -la
+tree /postgres_data
+
---
+++ delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#delete docker volumes
+rm -frv /postgres_data
